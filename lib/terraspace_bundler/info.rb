f3b3ec4c3287e562cf15354d8bd06aef313351ef
Grummfy <Grummfy@users.noreply.github.com>
2021-03-25 17:57:38 +0100

    terraspace bundle info: warning when no lockfile

--- lib/terraspace_bundler/info.rb
+++ lib/terraspace_bundler/info.rb
@@ -1,6 +1,13 @@
 module TerraspaceBundler
   class Info < TB::CLI::Base
     def run
+      file = TB.config.lockfile
+      unless File.exist?(file)
+        logger.info "No #{file} found".color(:red)
+        logger.info "Maybe run: terraspace bundle"
+        return
+      end
+      
       name = @options[:mod]
       found = lockfile.mods.find { |m| m.name == @options[:mod] }
       if found
